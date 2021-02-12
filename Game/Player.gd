57d7590005d65bbf8c097915b473f98eef8bb0d9
vaati3 <honorin.pouteau@gmail>
2021-02-12 20:51:09 +0100

    add animation

--- Game/Player.gd
+++ Game/Player.gd
@@ -7,16 +7,24 @@ var jumpForce : int = 420
 var gravity : int = 900
 var velocity : Vector2 =  Vector2.ZERO
 
-onready var sprite : Sprite = get_node("kevin_idle")
+onready var sprite : AnimatedSprite = get_node("Kevin")
 
 func _physics_process(delta):
 	velocity.x = 0
 	
 	if  Input.is_action_pressed("move_left"):
 		velocity.x -= speed
-	if Input.is_action_pressed("move_right"):
+	elif Input.is_action_pressed("move_right"):
 		velocity.x += speed
+	
+	if velocity.x == 0:
+		get_node("Kevin").stop()
+		get_node("Kevin").play("idle")
+	else:
+		get_node("Kevin").play("walk")
+	
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
 	
 	velocity.y += gravity * delta
 	if Input.is_action_just_pressed("jump") and is_on_floor():
