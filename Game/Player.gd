3050c9c711203179e8c545fc19123b8cf6ce83d4
vaati3 <honorin.pouteau@gmail>
2021-02-12 22:33:19 +0100

    add health, drink and hurt

--- Game/Player.gd
+++ Game/Player.gd
@@ -1,6 +1,8 @@
 extends KinematicBody2D
 
-var score : int = 0
+var maxHealth : int = 100
+var health : int = maxHealth
+var beer : int = 20
 
 var speed : int = 200
 var jumpForce : int = 420
@@ -9,6 +11,20 @@ var velocity : Vector2 =  Vector2.ZERO
 
 onready var sprite : AnimatedSprite = get_node("Kevin")
 
+var jumping : bool = false
+
+func drinkBeer():
+	get_node("Kevin").play("drink")
+	health = maxHealth if health + beer > maxHealth else health + beer
+
+func hurt(damage):
+	if health <= damage:
+		health = 0
+		get_node("Kevin").play("dead")
+	else:
+		health -= damage
+		get_node("Kevin").play("hurt")
+
 func _physics_process(delta):
 	velocity.x = 0
 	
@@ -16,20 +32,23 @@ func _physics_process(delta):
 		velocity.x -= speed
 	elif Input.is_action_pressed("move_right"):
 		velocity.x += speed
-	
-	
 	velocity = move_and_slide(velocity, Vector2.UP)
 	
 	if is_on_floor():
+		jumping = false
 		if velocity.x == 0:
 			get_node("Kevin").play("idle")
 		else:
 			get_node("Kevin").play("walk")
 	
 	velocity.y += gravity * delta
+	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y -= jumpForce
 		get_node("Kevin").play("jump")
+		jumping	= true
+	if !is_on_floor() and !jumping:
+		get_node("Kevin").play("fall")
 	
 	if velocity.x < 0:
 		sprite.flip_h = true
