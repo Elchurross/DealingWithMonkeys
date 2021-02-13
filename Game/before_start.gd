b9fc5602a645341e37ff2db9f3e01ede66980407
Louis Boulet <louis.boulet@epitech.eu>
2021-02-13 13:56:11 +0100

    menu and other
    menu and other

---
+++ Game/before_start.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+var pause:bool = true
+export var reference_path = ""
+func _ready() -> void:
+	yield(get_tree().create_timer(1.0), "timeout")
+	pause = false
+
+func _process(delta:float) -> void:
+	if !pause:
+		var up:bool = Input.is_action_just_pressed("jump")
+
+		if up:
+			if(reference_path != ""):
+				get_tree().change_scene(reference_path)
+
