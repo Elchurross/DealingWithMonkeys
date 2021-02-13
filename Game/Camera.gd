aaa3fc7f1eeafad6414bc34452ccca6feead3587
titouan01 <56084062+titouan01@users.noreply.github.com>
2021-02-13 19:44:16 +0100

    Add beginning design level1

--- Game/Camera.gd
+++ Game/Camera.gd
@@ -4,7 +4,7 @@ extends Camera2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var old_global_pos = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,6 +15,8 @@ func _ready():
 func _process(_delta):
 	var player = get_owner().get_node("Player")
 	global_position.y = 190
-	global_position.x = player.get_global_position().x
-
+	if player.get_global_position().x < old_global_pos:
+		global_position.x = old_global_pos
+	else:
+		global_position.x = player.get_global_position().x
 	pass
